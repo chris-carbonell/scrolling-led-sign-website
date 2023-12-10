2a668c20538bf02d69dd3149b447dfd4e78bd3fd
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-09 23:13:49 -0600

    feature: add metadata and template for get_text

--- api/main.py
+++ api/main.py
@@ -10,17 +10,60 @@ import os
 from fastapi import FastAPI
 
 # data
-import psycopg2
-from psycopg2 import Error
+from utils.conn import connection
 
 # app
 app = FastAPI()
 
 # endpoints
 
-@app.get("/healthcheck")
+## metadata
+tags_metadata = [
+    {"name": "admin", "description": "admin tools"},
+    {"name": "get", "description": "get data"},
+    {"name": "put", "description": "put data"},
+]
+
+## admin
+
+@app.get("/healthcheck", tags=["admin"])
 async def root():
     '''
     health check for container
     '''
+    # TODO: return HTTP error if necessary table doesnt exist
     return
+
+## get
+
+@app.get("/text", tags=["get"])
+async def get_text(types: str = None, asc: bool = True):
+    '''
+    get text
+
+    types: type of text to get; string of comma-separated types (e.g., "sports, random")
+    asc: order messages aescending (i.e., True = oldest messages at the top of the queue)
+    '''
+
+    # get sql
+    sql = "SELECT text FROM texts"
+    if types:
+        sql += f" WHERE type IN ({types})"
+    sql += " ORDER BY dt_entered"
+    if asc:
+        sql += " ASC"
+    else:
+        sql += " DESC"
+    sql += ";"
+
+    # get text
+    with connection() as conn:
+        curs = conn.cursor()
+        curs.execute(sql)
+        record = curs.fetchone()
+        text = record[0]
+
+    return {'text': text}
+
+# TODO: put to add text (for front end)
+# TODO: get n texts asc/desc for checking in on people
