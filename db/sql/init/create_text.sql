b350a45b7d3a7b26b5ac58934cb0b1b0294f61ad
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-10 01:17:54 -0600

    feature: reorg db so we cn use Dockerfile

---
+++ db/sql/init/create_text.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS texts (
+    text_id SERIAL PRIMARY KEY,
+    dt_entered TIMESTAMP WITH TIME ZONE,
+    dt_requested TIMESTAMP WITH TIME ZONE,
+    client_host VARCHAR(16),
+    text_source VARCHAR(255),
+    text_tags VARCHAR(255) ARRAY,
+    text TEXT
+);
