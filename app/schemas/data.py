f589a21dc8c4fac7dafb2444b262f4c9caf7d69c
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-11 23:40:35 -0600

    fix: table headers

--- app/schemas/data.py
+++ app/schemas/data.py
@@ -3,8 +3,6 @@ from pydantic import BaseModel, Field
 from typing import Annotated, List
 
 class TextRecord(BaseModel):
-    # TODO: the table gets Dt Entered, can we control the name of that here somehow?
-    # TODO: or maybe with DisplayLookup like in the cities table in the demo
     dt_entered: datetime | None = Field(title="dt_entered")
     dt_requested: datetime | None = Field(title="dt_requested")
     client_host: str | None = Field(title="client_host")
