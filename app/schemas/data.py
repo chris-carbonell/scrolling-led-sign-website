81a5f648c6c5d5ac9b990eada84376c88fd86bd0
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-11 23:40:15 -0600

    fix: table headers

--- app/schemas/data.py
+++ app/schemas/data.py
@@ -5,9 +5,9 @@ from typing import Annotated, List
 class TextRecord(BaseModel):
     # TODO: the table gets Dt Entered, can we control the name of that here somehow?
     # TODO: or maybe with DisplayLookup like in the cities table in the demo
-    dt_entered: datetime | None
-    dt_requested: datetime | None
-    client_host: str | None
-    text_source: str | None
-    text_tags: List[str] | None
-    text: str
+    dt_entered: datetime | None = Field(title="dt_entered")
+    dt_requested: datetime | None = Field(title="dt_requested")
+    client_host: str | None = Field(title="client_host")
+    text_source: str | None = Field(title="text_source")
+    text_tags: List[str] | None = Field(title="text_tags")
+    text: str = Field(title="text")
