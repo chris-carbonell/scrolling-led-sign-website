a661ab69668923704f475da4098801e3bc301d57
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-21 20:03:14 -0600

    feature: basic form

--- app/app.py
+++ app/app.py
@@ -1,4 +1,19 @@
+# Overview
+# frontend app for interacting with database
+
+# Dependencies
+
+# app
 import streamlit as st
 
-st.title("hello world")
-st.text_input("test")
+# App
+
+# headers
+st.title("Scrolling LED Sign")
+
+# form
+with st.form("my_form", clear_on_submit=True):
+    text = st.text_input("send a message")
+    submitted = st.form_submit_button("Submit")
+
+# process
