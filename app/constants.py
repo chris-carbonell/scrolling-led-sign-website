5a903b31758cbb0a09f9e1e7605ac6dae932a53e
Chris Carbonell <ccarbonell228@gmail.com>
2023-12-30 20:42:33 -0600

    fix: use shorter alias

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,3 @@
 # use `docker inspect` to get the alias of the database container
 # it's also the container name or the service name
-URL_API = "http://scrolling-led-sign-website-api:8000"
+URL_API = "http://db:8000"
