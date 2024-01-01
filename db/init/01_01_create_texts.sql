4b855819a207cdb9ea5d256ec48bcf0f8498e94b
Chris Carbonell <ccarbonell228@gmail.com>
2024-01-01 09:15:10 -0600

    fix: add name to texts

--- db/init/01_01_create_texts.sql
+++ db/init/01_01_create_texts.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS texts (
     dt_requested TIMESTAMP WITH TIME ZONE,
     client_host VARCHAR(16),
     text_source VARCHAR(255),
+    name VARCHAR(255),
     text_tags VARCHAR(255) ARRAY,
     text TEXT
 );
