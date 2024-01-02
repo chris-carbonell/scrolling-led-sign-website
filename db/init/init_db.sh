280767a76e3a074227b696182c9b8af06cb5df7f
Chris Carbonell <ccarbonell228@gmail.com>
2024-01-01 19:57:00 -0600

    fix: make access codes unique

--- db/init/init_db.sh
+++ db/init/init_db.sh
@@ -37,7 +37,7 @@ execute "
     CREATE TABLE IF NOT EXISTS codes (
         code_id SERIAL PRIMARY KEY,
         dt_entered TIMESTAMP WITH TIME ZONE,
-        code VARCHAR(255),
+        code VARCHAR(255) UNIQUE,
         is_admin BOOLEAN
     );
 "
